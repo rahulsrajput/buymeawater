319e503d095e36fc5616474e1d9ccfc8ec553b77
rahulsrajput <rahulsrajput29@gmail.com>
2024-04-02 08:57:11 +0530

    db changed

--- buymewater/settings.py
+++ buymewater/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'buymewater.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': os.environ.get("PASSWORD"),
+        'HOST': 'monorail.proxy.rlwy.net',
+        'PORT': os.environ.get("PORT"),
     }
 }
 
